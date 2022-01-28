3e5dbe8148a93309904c6311b53f7e0edd8d0da2
Gal1le0 <elias_lichaa@outlook.com>
2022-01-28 12:18:24 +0200

    chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,35 @@
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	useLocation,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
 } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Socket from './pages/Socket'
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
-	const location = useLocation
+  const location = useLocation
 
-	return (
-		<Router>
-			<div className='App'>
-				<div className='pages'>
-					<Switch location={location.location} key={location.path}>
-						<Route exact path='/'>
-							<Home />
-						</Route>
-					</Switch>
-				</div>
-			</div>
-		</Router>
-	)
+  return (
+    <Router>
+      <div className="App">
+        <div className="pages">
+          <Switch location={location.location} key={location.path}>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/socket">
+              <Socket />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/Socket.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+// import Messages from './Messages'
+// import MessageInput from './MessageInput'
+
+function Socket() {
+  const [socket, setSocket] = useState(null)
+  const [message, setMessage] = useState([])
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    const newSocket = io(`http://localhost:5000`)
+    setSocket(newSocket)
+    return () => newSocket.close()
+  }, [setSocket])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('message', (msg) => {
+        console.log(msg)
+        setMessage((messages) => {
+          let newMessages = messages.slice()
+          newMessages.push({ msg, type: 'receive' })
+          return newMessages
+        })
+      })
+    }
+  }, [socket])
+
+  const style = {
+    default: {
+      color: 'White',
+      width: 'max-content',
+      maxWidth: '60%',
+      padding: 10,
+      borderRadius: 40,
+      marginTop: 4,
+      marginBottom: 4,
+    },
+    button: {
+      color: 'white',
+      padding: '4px 8px',
+      backgroundColor: value ? 'blue' : 'grey',
+      width: 'max-content',
+      marginTop: 8,
+      cursor: value ? 'pointer' : '',
+    },
+    send: {
+      backgroundColor: 'Green',
+      borderTopLeftRadius: 'unset',
+    },
+    receive: {
+      backgroundColor: 'Blue',
+      marginLeft: 'auto',
+      borderTopRightRadius: 'unset',
+    },
+  }
+  return (
+    <div className="App">
+      <header className="app-header">React Chat</header>
+      {socket ? (
+        <div className="chat-container">
+          <input
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value)
+            }}
+          />
+          <div
+            onClick={() => {
+              if (value) {
+                setMessage((messages) => {
+                  let newMessages = messages.slice()
+                  newMessages.push({ type: 'send', msg: value })
+                  return newMessages
+                })
+                socket.send(value)
+                setValue('')
+              }
+            }}
+            style={style.button}
+          >
+            SEND
+          </div>
+          {/* <div
+            onClick={() => {
+              setMessage((messages) => {
+                let newMessages = messages.slice()
+                newMessages.push({ type: 'send', msg: 'my name' })
+                return newMessages
+              })
+              socket.send('my name')
+            }}
+          >
+            hello
+          </div>
+          <div
+            onClick={() => {
+              setMessage((messages) => {
+                let newMessages = messages.slice()
+                newMessages.push({ type: 'send', msg: 'bye' })
+                return newMessages
+              })
+              socket.send('boy')
+            }}
+          >
+            bye
+          </div> */}
+          <div>
+            {message.map((msg) => {
+              return (
+                <div style={{ ...style.default, ...style[msg.type] }}>
+                  {msg.msg}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      ) : (
+        <div>Not Connected</div>
+      )}
+    </div>
+  )
+}
+
+export default Socket
